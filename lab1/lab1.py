2f970361c0d627db0a16e45b286f9d9520911df6
Yang-Xavier <xavier_ybx@163.com>
2019-02-18 10:34:18 +0000

    Optimize and Update Report

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -92,16 +92,21 @@ def divide_data(data, valid_num, random=False):
     return training_data,valid_data
 
 # Do test for the training weight
-def validation(weight, valid_data):
-    positive_data = valid_data[np.where(valid_data[:,0]>0)[0]]
-    negative_data = valid_data[np.where(valid_data[:,0]<0)[0]]
-    pre_positive = np.sign(np.dot(np.delete(positive_data, [0], axis=1), weight))
-    pre_negative = np.sign(np.dot(np.delete(negative_data, [0], axis=1), weight))
+def validation(weight,  valid_files, uniq_keys, approach):
+
+    positive_data,pk = extract_feature(valid_files[:int(valid_files.shape[0]/2)], SYMBOL, POS, approach)
+    negative_data,nk = extract_feature(valid_files[int(valid_files.shape[0]/2):], SYMBOL, NEG, approach)
+
+    valid_positive_data = build_matrix(positive_data,uniq_keys)
+    valid_negative_data = build_matrix(negative_data,uniq_keys)
+
+    pre_positive = np.sign(np.dot(np.delete(valid_positive_data, [0], axis=1), weight))
+    pre_negative = np.sign(np.dot(np.delete(valid_negative_data, [0], axis=1), weight))
 
     true_positive = (pre_positive>0).sum()
-    false_positive = positive_data.shape[0] - true_positive
+    false_positive = len(positive_data) - true_positive
     true_negative = (pre_negative < 0).sum()
-    false_negative = negative_data.shape[0] - true_negative
+    false_negative = len(negative_data) - true_negative
 
     precision = true_positive/(true_positive+false_positive)
     recall = true_positive/(true_positive + false_negative)
@@ -117,11 +122,11 @@ def print_top_ten(weight, keys):
     for i in range(10):
         print("%s: %.4f \t %s: %.4f" % (keys[positive_top_ten[(9-i)]+1][0], weight[positive_top_ten[(9-i)]], keys[negative_top_ten[i]+1][0], weight[negative_top_ten[i]]))
 
-def run(approach):
+def run(approach,  train_files, valid_files):
     start = time.clock()
     print('\n----------------------------------%s-----------------------------------------'%(approach))
-    pf, pkeys = extract_feature(pos_files, SYMBOL, POS, approach)
-    nf, nkeys = extract_feature(neg_files, SYMBOL, NEG, approach)
+    pf, pkeys = extract_feature(train_files[:int(train_files.shape[0]/2)], SYMBOL, POS, approach)
+    nf, nkeys = extract_feature(train_files[int(train_files.shape[0]/2):], SYMBOL, NEG, approach)
     uniq_keys = np.unique(np.append(pkeys, nkeys))
     i = np.where(uniq_keys == SYMBOL)[0][0]
     uniq_keys[[0, i]] = uniq_keys[[i, 0]]
@@ -130,45 +135,39 @@ def run(approach):
     print('Cost of Extracting feature: %.2fs' % (time.clock() - start))
     start = time.clock()
 
-    data_matrix = build_matrix(np.append(pf, nf), uniq_keys)
+    training_data_matrix = build_matrix(np.append(pf, nf), uniq_keys)
     print('Cost of Building matrix: %.2fs' % (time.clock() - start))
     print('The length of the features:  ', uniq_keys.shape[0])
     start = time.clock()
 
-    training_data, valid_data = divide_data(data_matrix, VALID_NUM)
-    print('Cost of Dividing training and valid data: %.2fs' % (time.clock() - start))
-    start = time.clock()
-
     #  start iteration
 
-    weight = np.zeros((training_data.shape[1] - 1, 1))
-    signs = training_data[:, 0].reshape((training_data.shape[0], 1))
+    weight = np.zeros((training_data_matrix.shape[1] - 1, 1))
     MAX_ITERATION = 10
-    error = training_data.shape[0]
 
     print('Start Training-----------------------------------------------------------------------------')
 
-    training_data_ = training_data  # backup training data
+    training_data_ = training_data_matrix  # backup training data
 
     for i in range(MAX_ITERATION):
         np.random.seed(i)
-        training_data = np.random.permutation(training_data_)
-        signs = training_data[:, 0].reshape((training_data.shape[0], 1))
-        training_data = training_data[:, 1:]
+        training_data_matrix = np.random.permutation(training_data_)
+        signs = training_data_matrix[:, 0].reshape((training_data_matrix.shape[0], 1))
+        training_data_matrix = training_data_matrix[:, 1:]
 
-        for j in range(training_data.shape[0]):
-            pre = 1 if np.sign(np.dot(training_data[j], weight))[0] >= 0 else -1
+        for j in range(training_data_matrix.shape[0]):
+            pre = 1 if np.sign(np.dot(training_data_matrix[j], weight))[0] >= 0 else -1
             if pre != signs[j]:
-                weight += (signs[j] * training_data[j]).reshape(weight.shape[0], 1)
-        pre_all = np.sign(np.dot(training_data, weight))
+                weight += (signs[j] * training_data_matrix[j]).reshape(weight.shape[0], 1)
+        pre_all = np.sign(np.dot(training_data_matrix, weight))
         error = (np.abs(signs - pre_all)).sum()
         print("Iteraton %d : Error  %d  " % (i + 1, error))
 
-    weight /= training_data.shape[0]
+    weight /= training_data_matrix.shape[0]
     print('End Training-------------------------------------------------------------------------------')
     print('Cost of Training: %.2fs' % (time.clock() - start))
 
-    true_positive, true_negative, precision, recall = validation(weight, valid_data)
+    true_positive, true_negative, precision, recall = validation(weight, valid_files, uniq_keys, approach)
     print("TruePositive: %d \nTrueNegative: %d \nPrecision: %.2f \nRecall: %.2f \n" % (
     true_positive, true_negative, precision, recall))
 
@@ -195,12 +194,16 @@ FEATURE_TYPE = 'all' if  not args.t else args.t
 pos_files = read_files(pos_folder)
 neg_files = read_files(neg_folder)
 
+valid_files = np.append(pos_files[:int(VALID_NUM/2)], neg_files[:int(VALID_NUM/2)])
+train_files = np.append(pos_files[int(VALID_NUM/2):], neg_files[int(VALID_NUM/2):])
+
+
 print('Cost of Reading files: %.2fs'%(time.clock()-start))
 start = time.clock()
 
 if FEATURE_TYPE is not 'all':
-    run(FEATURE_TYPE)
+    run(FEATURE_TYPE,  train_files, valid_files)
 else:
-    run('unigram')
-    run('bigram')
-    run('trigram')
+    run('unigram', train_files, valid_files)
+    run('bigram',  train_files, valid_files)
+    run('trigram',  train_files, valid_files)
